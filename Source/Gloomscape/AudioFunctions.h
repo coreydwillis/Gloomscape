96f88802859efebc1d80b72ca7064a3f324a9a5a
Corey Willis <coreydwillis@gmail.com>
2023-05-28 13:53:58 -0600

    Other code changes

---
+++ Source/Gloomscape/AudioFunctions.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "AudioFunctions.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GLOOMSCAPE_API UAudioFunctions : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+        //* Declare blueprint functions here?
+        UFUNCTION(BlueprintCallable, Category = "AudioFunctions", meta = (DisplayName = "Get Sound Wave from Wave file", Keywords = "Get Sound Wave from Wave file"))
+        static class USoundWave* GetSoundFromWaveFile(const FString& filePath, bool& Success);
+
+
+    /** Obtain all files in a provided directory, with optional extension filter. All files are returned if Ext is left blank. Returns false if operation could not occur. */
+    UFUNCTION(BlueprintPure, Category = "AudioFunctions")
+        static bool GetALLFilesInFolder(TArray<FString>& Files, FString RootFolderFullPath, FString Ext);
+
+};
